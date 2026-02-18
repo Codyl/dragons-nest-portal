29ddb797339010f7ec65f77ba9bf1ec23e9cadc9
codygettuneski <cody@gettuneski.com>
2026-02-18 09:45:52 -0700

    feat: add health check before each test to ensure server readiness
    - Implemented a beforeEach hook in Cypress tests to perform a health check on the test server.
    - Validated server response status and environment to ensure tests run in the correct context, improving test reliability.

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -3,6 +3,17 @@
 import './commands';
 import 'cypress-storybook/cypress';
 
+beforeEach(() => {
+  cy.request('GET', 'http://localhost:8080/health').then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Failed to ping test server');
+    }
+    if (response.body.debug.nodeEnv !== 'test') {
+      throw new Error('Node environment is not test');
+    }
+  });
+});
+
 afterEach(() => {
   cy.clearLocalStorage();
   cy.clearCookies();
