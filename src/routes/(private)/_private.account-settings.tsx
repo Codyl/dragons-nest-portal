012cee796fa604fc18ca81963fada619fb6f404f
cody <codylillyw@gmail.com>
2026-05-06 13:50:11 -0600

    refactor: remove legacy account settings route

--- src/routes/(private)/_private.account-settings.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/(private)/_private/account-settings')({
-  beforeLoad: () => {
-    throw redirect({ to: '/settings/profile', replace: true });
-  },
-});
